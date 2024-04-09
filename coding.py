f80f76c8a51bcb736936e9c0f74e4555b1bbe7a8
Shiyam Rupeshkumar <shiyamd@terpmail.umd.edu>
2024-04-09 17:29:17 -0400

    Working size, push, pop and developer commands version; implementing a pops function is next step

--- coding.py
+++ coding.py
@@ -6,17 +6,43 @@ class Stack:
         self.stack = np.empty(size)
         #self.top is a running index for the top of the stack; index starts with 0
         self.top = size - 1
+        self.maxsize = size
+        self.size = 0
+    
+    def size(self):
+        return self.size
     
     def push(self, val):
-        self.stack[self.top] = val
-        self.top -= 1
+        if self.top == -1:
+            return False
+        else:
+            self.stack[self.top] = val
+            self.top -= 1
+            self.size += 1
+            return True
 
     def pop(self):
-        self.top += 1
-        return self.stack[self.top]
-        
+        if self.top == self.size - 1:
+            return None
+        else: 
+            #if this line below just returned the popped value then the 2 lines below it wouldn't work
+            #so I saved the value in a temp variable for later use as the top index will change
+            popped_value = self.stack[self.top+1]
+            self.top += 1
+            self.size -= 1
+            return popped_value
     
-
-
+    
+    
+            
+    
+    def debug(self):
+        return (self.top, self.maxsize, self.stack[self.top])
+    
+    def debug2(self):
+        temp = []
+        for i in range(self.size):
+            temp.append(self.stack[i])
+        return temp
 
 
