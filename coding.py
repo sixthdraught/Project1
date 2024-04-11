1f8c00d97b2807bc612df48acc9f227ada9e6990
Shiyam Rupeshkumar <shiyamd@terpmail.umd.edu>
2024-04-10 20:02:52 -0400

    tentatively finished OQ implementation with comments

--- coding.py
+++ coding.py
@@ -44,7 +44,7 @@ class Stack:
     def pops(self, val):
         #checks if the value given is an integer and returns a helpful message if it isn't
         if not isinstance(val, int):
-            return " the function pops() only accepts integers"
+            return "the function pops() only accepts integers"
         #checks if the stack is empty
         elif self.top == self.maxsize - 1:
             return []
@@ -207,9 +207,9 @@ class OQ:
         #self.first is an index for the front of the queue, it starts at 0 as numpy arrays start at 0 and it 
         #stays 0
                 self.first = 0
-        #keeps track of the back of the queue
+        #keeps track of the back of the queue(only here for the gets function)
                 self.last = 0
-        #variable for maxsize, used for debugging
+        #variable for maxsize
                 self.maxsize = size
         #variable for current size
                 self.csize = 0
@@ -229,51 +229,52 @@ class OQ:
             return False
         #makes sure the OQ isn't empty
         elif self.csize > 0:
-        #iterates the forecoming code to make sure 
+        #iterates the forecoming code to make sure all values in the oq are checked against the current value
             for i in range(self.csize):
+                #checks if val is low enough to be inserted at the current index in the oq
                 if val < self.oq[i]:
+                    #stores the index at which the value will be added while the values behind and at that index are shuffled back
                     temp = i
+                    #this for loop is used to shuffle back all values at and behind the index the new value will be inserted at
+                    #the values at the back must be moved first which is why there is a reversed()
                     for i_2 in reversed(range(i, self.csize)):
+                    #moves a value back in the queue
                         self.oq[i_2+1] = self.oq[i_2]
+                    #takes the stored index and inserts the value at the stored index
                     self.oq[temp] = val
+                    #updates csize
                     self.csize += 1
+                    #updates where the back of the queue is
+                    self.last += 1
                     return True
-    
+            #this chunk handles the case in which the value is the new largest number
             self.oq[self.csize] = val
             self.csize += 1
+            self.last += 1
             return True
+        #this chunk handles the case in which the stack is empty in which case the value is inserted at the front
         elif self.csize == 0:
             self.oq[self.first] = val  
             self.csize += 1  
+            self.last += 1
             return True
-        
-        #developer commands
-    def debug(self):
-        #returns useful variables in the specified stack
-        return (self.csize, self.maxsize, self.debug2())
     
-    def debug2(self):
-        #returns all values in the specified queue
-        temp = []
-        for i in range(self.maxsize):
-            temp.append(self.oq[i])
-        return temp    
-    
-
+    #the following functions are almost exactly the same as their respective implementations in the class FIFO
+    #only differences are a few variable names
     def get(self):
         #checks if the queue is empty
         if self.csize == 0:
             return None
         else: 
             #removes the first value
-            removed_value = self.fifo[self.first]
+            removed_value = self.oq[self.first]
             #moves the whole queue up by one to compensate for the first value's removal
             for i in range(self.csize):
                 if not(i == self.maxsize-1):
-                    self.fifo[i] = self.fifo[i+1]
+                    self.oq[i] = self.oq[i+1]
             #updates the last index
             self.last -= 1
-            self.fifo[self.last] = None
+            self.oq[self.last] = None
             #updates the csize count
             self.csize -= 1
             return removed_value
@@ -288,20 +289,20 @@ class OQ:
             #iterates this block over the whole queue as the given value is too big
             for i in range(self.csize):
                 #stores the first value before it's modified by the next for loop
-                removed_value = self.fifo[self.first]
+                removed_value = self.oq[self.first]
                 #loop that shifts the whole queue up by one to account for numpy arrays
                 #having a fixed size
                 for i in range(self.csize):
                     #checks for the case where the resulting index called by i+1 is out of bounds
                     if not(i == self.maxsize-1):
                         #shifts the selected index up by 1
-                        self.fifo[i] = self.fifo[i+1]
+                        self.oq[i] = self.oq[i+1]
                 #takes the stored value and appends it to a list to return to the user later
                 temp.append(removed_value)
                 #updates the last index
                 self.last -= 1
                 #accounts for the last value not being removed by the previous for loop
-                self.fifo[self.last] = None
+                self.oq[self.last] = None
                 #updates the csize count
                 self.csize -= 1
             return temp
@@ -311,20 +312,32 @@ class OQ:
             #iterates this block over the given int to return the requested number of values from the queue
             for i in range(val):
                 #stores the first value before it's modified by the next for loop
-                removed_value = self.fifo[self.first]
+                removed_value = self.oq[self.first]
                 #loop that shifts the whole queue up by one to account for numpy arrays
                 #having a fixed size
                 for i in range(self.csize):
                     #checks for the case where the resulting index called by i+1 is out of bounds
                     if not(i == self.maxsize-1):
                         #shifts the selected index up by 1
-                        self.fifo[i] = self.fifo[i+1]
+                        self.oq[i] = self.oq[i+1]
                 #takes the stored value and appends it to a list to return to the user later
                 temp.append(removed_value)
                 #updates the last index
                 self.last -= 1
                 #accounts for the last value not being removed by the previous for loop
-                self.fifo[self.last] = None
+                self.oq[self.last] = None
                 #updates the csize count
                 self.csize -= 1
             return temp
+        
+    #developer commands
+    def debug(self):
+        #returns useful variables in the specified stack and runs debug2()
+        return (self.csize, self.maxsize, self.debug2())
+    
+    def debug2(self):
+        #returns all values in the specified queue
+        temp = []
+        for i in range(self.maxsize):
+            temp.append(self.oq[i])
+        return temp    
