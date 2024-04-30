5593265d10712c306361750323293edb67bfdd35
Shiyam Rupeshkumar <shiyamd@terpmail.umd.edu>
2024-04-29 20:38:12 -0400

    same commit as previous with a trees file added

--- trees_graphs_nodes.py
+++ trees_graphs_nodes.py
@@ -1,3 +1,12 @@
-class Trees:
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.left = None
+        self.val = val
+
+class Tree:
     def __init__(self):
         
+        
+class Graph:
+    def __init__self
